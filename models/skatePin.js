495e3dfe2873df81a2b5a2120f9821246ed7a39c
Jack <Jackw3434@gmail.com>
2020-03-28 00:18:36 +0000

    removed _id Path from skate pin

--- models/skatePin.js
+++ models/skatePin.js
@@ -2,7 +2,7 @@ let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
 
 let SkatePinSchema = new Schema({
-  _id: { type: mongoose.Types.ObjectId, required: true },
+ // _id: { type: mongoose.Types.ObjectId, required: true },
   title: { type: String, required: true, },
   createdBy: { type: String, required: true },
   coordinate: {
